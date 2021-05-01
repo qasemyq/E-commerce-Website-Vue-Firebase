69e0f5ad1898b239d2202222a9948870d0caa245
Ghader Ghasemi <68222268+qasemyq@users.noreply.github.com>
2021-05-01 16:13:53 +0200

    Update Footer.vue

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -12,12 +12,10 @@
         ><i class="fab fa-facebook-f"></i
       ></a>
 
-      <!-- Twitter -->
       <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"
         ><i class="fab fa-twitter"></i
       ></a>
 
-      <!-- Google -->
       <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"
         ><i class="fab fa-google"></i
       ></a>
@@ -27,22 +25,17 @@
         ><i class="fab fa-instagram"></i
       ></a>
 
-      <!-- Linkedin -->
       <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"
         ><i class="fab fa-linkedin-in"></i
       ></a>
 
-      <!-- Github -->
       <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"
         ><i class="fab fa-github"></i
       ></a>
     </section>
-    <!-- Section: Social media -->
 
-    <!-- Section: Form -->
     <section class="">
       <form action="">
-        <!--Grid row-->
         <div class="row d-flex justify-content-center">
        
          
